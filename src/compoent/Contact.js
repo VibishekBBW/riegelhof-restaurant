ed4abcc261119ec12a1487db9e727570a36660e1
VibishekBBW <vibishek.rathirajan@lernende.bbw.ch>
2024-02-27 22:46:59 +0100

    Fix font

--- src/compoent/Contact.js
+++ src/compoent/Contact.js
@@ -4,8 +4,14 @@ const Contact = () => {
   return (
     <div className="home-div">
       <div className='main-div'>
-        <h1>Adresse</h1>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2690.0918025025003!2d8.50387507680811!3d47.60490458809398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479070a9b94d00f1%3A0xd0c5e72038c53b07!2sGasthaus%20zum%20Riegelhof!5e0!3m2!1sde!2sch!4v1708982769722!5m2!1sde!2sch" width="400" height="450" className="map" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" title="riegelhof-map"></iframe>
+        <br/>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2690.
+          0918025025003!2d8.50387507680811!3d47.60490458809398!2m3!1f0!2f0!3f0!3m2!1i
+          1024!2i768!4f13.1!3m3!1m2!1s0x479070a9b94d00f1%3A0xd0c5e72038c53b07!2sGasthaus%20
+          zum%20Riegelhof!5e0!3m2!1sde!2sch!4v1708982769722!5m2!1sde!2sch" 
+          width="400" height="450" className="map" allowFullScreen loading="lazy" 
+          referrerPolicy="no-referrer-when-downgrade" title="riegelhof-map"></iframe>
+        <br/>
         <br/>
         <div className='adr'>
         <h2>Restaurant Riegelhof</h2>
--- src/compoent/Menu.js
+++ src/compoent/Menu.js
@@ -3,9 +3,9 @@ import pdf from '../pdf/logo.pdf';
 
 const Menu = () => {
   return (
-    <>
+    <div className="main-div">
           <iframe src={pdf} width="100%" style={{ height: '90vh' }} title="riegelhof-menu"/>
-    </>
+    </div>
   )
 }
 export default Menu
